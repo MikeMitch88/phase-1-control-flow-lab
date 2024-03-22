c76c9621506ec619f6410fcaa1b764957f5e9f59
Randa_Mike <michaelranda95@gmail.com>
2024-03-22 16:19:37 +0300

    commited a check,distatnce

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+if (distance <= 400)
+{
+  return 'This one is on me!'
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+else if(distance > 400 && distance < 2000){
+return'That will be twenty bucks.'
 }
-
-function switchOnCharmFromTip(){
+else if (distance > 2000 && distance <= 2500){
+  return 'I will gladly take your thirty bucks.'
+}
+else if(distance > 2500){
+  return 'No can do.'
+}
+}
+function ternaryCheckCity(city, result){
   // Write your code here!
+
+     result = city ==='NYC' ? "Ok, sounds good." : "No go."
+    return result
+  }
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+
+    default:
+      return 'Bye.'
+  }
 }
